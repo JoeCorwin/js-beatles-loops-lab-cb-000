ee466a5a5b132b818f4fca955fcc1ce6267ad77f
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 19:38:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var beatlesPlay = []
 
 function theBeatlesPlay(musicians, instruments) {
-  for (let n = 0; n++) {
+  for (let n = 0; n < musicians.length; n++) {
     beatlesPlay.push(`${musicians[n]} plays ${instruments[n]}`)
   }
   return beatlesPlay
 }
 
 function johnLennonFacts(facts) {
-  
+
 }
-//n < musicians.length; 
+
